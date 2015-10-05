154a231b6d4c3009ca7352fe7c10271e100308f1
Sam Vecht <samvecht@gmail.com>
2015-10-05 10:11:08 -0700

    test3

--- app/src/main/java/edu/uw/tacoma/mmuppa/helloworld/MainActivity.java
+++ app/src/main/java/edu/uw/tacoma/mmuppa/helloworld/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//
         Log.i(TAG, "onCreate method");
         sendAgainButton = (Button)
                 findViewById(R.id.btn_send_again);
